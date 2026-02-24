8fc4f6366fdccd8d6c146b73a660dfe1fa8c347a
Michael <michaelkuen888@gmail.com>
2026-02-24 17:28:13 +0200

    fixes-3

--- lib/database/web.dart
+++ lib/database/web.dart
@@ -1,8 +1,24 @@
+import 'package:drift/drift.dart';
 import 'package:drift/wasm.dart';
 import 'package:flutter/foundation.dart';
 
-QueryExecutor openConnection() {
-  return WasmDatabase.supported()
-      ? WasmDatabase(dbName: 'db')
-      : WasmDatabase.serializable();
+DatabaseConnection openConnection() {
+  return DatabaseConnection.delayed(Future(() async {
+    final result = await WasmDatabase.open(
+      databaseName: 'db',
+      sqlite3Uri: Uri.parse('sqlite3.wasm'),
+      driftWorkerUri: Uri.parse('drift_worker.js'),
+    );
+
+    if (result.missingFeatures.isNotEmpty) {
+      // Depending on your application's requirements, you may want to notify
+      // the user that some features are not available.
+      if (kDebugMode) {
+        print('Using ${result.chosenImplementation} due to unsupported '
+            'features: ${result.missingFeatures}');
+      }
+    }
+
+    return result.resolvedExecutor;
+  }));
 }
